006b3301278f1840a2e83b91840cab697e9a733e
Peiso Simone <simonepeiso@gmail.com>
2024-02-05 20:58:07 +0200

    Added EskomCalendar Models

---
+++ Model/OutagesRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadshedding.Model
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class OutagesRoot
+    {
+        public string AreaName { get; set; }
+        public int Stage { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finsh { get; set; }
+        public string Source { get; set; }
+    }
+}
